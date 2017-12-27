5530e89373b818624e9c8812622f89d15da17060
wty <1315514009>
2017-12-27 14:50:17 +0800

    DecoratorPattern装饰者模式 IO流test

---
+++ src/DecoratorPattern/IO/InputTest.java
@@ -0,0 +1,26 @@
+package DecoratorPattern.IO;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * @Description:
+ * @Author: wty
+ * @Date: Created in 14:39 2017/12/27
+ */
+public class InputTest {
+    public static void main(String[] args) {
+        int c;
+        try {
+            InputStream inputStream = new LowCaseInputStream(new BufferedInputStream(new FileInputStream("Files/DecoratorPatternIOTest.txt")));
+            while ((c = inputStream.read()) >= 0) {
+                System.out.println((char)c);
+            }
+            inputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
